8137fbd6b3c7f06580b68759e313b52939b847c1
Dinuka Werasingha <93566725+wptdinuka@users.noreply.github.com>
2022-01-27 21:13:18 +0100

    Add files via upload

---
+++ JPanal.java
@@ -0,0 +1,48 @@
+import java.awt.BorderLayout;
+import java.awt.Color;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class JPanal {
+
+    public static void main(String[] args) {
+
+        // JPanel = a GUI component that functions as a container to hold other components
+        ImageIcon icon = new ImageIcon("icon.jpg");
+
+
+        JLabel label = new JLabel();
+        label.setText("Hi");
+        //label.setIcon(icon);
+        label.setVerticalAlignment(JLabel.TOP);
+        label.setHorizontalAlignment(JLabel.LEFT);
+        label.setSize(5, 5);
+
+        JPanel redPanel = new JPanel();
+        redPanel.setBackground(Color.red);
+        redPanel.setBounds(0, 0, 250, 250);
+        redPanel.setLayout(new BorderLayout());
+
+        JPanel bluePanel = new JPanel();
+        bluePanel.setBackground(Color.blue);
+        bluePanel.setBounds(250, 0, 250, 250);
+        bluePanel.setLayout(new BorderLayout());
+
+        JPanel greenPanel = new JPanel();
+        greenPanel.setBackground(Color.green);
+        greenPanel.setBounds(0, 250, 500, 250);
+        greenPanel.setLayout(new BorderLayout());
+
+        JFrame frame = new JFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLayout(null);
+        frame.setSize(850,750);
+        frame.setVisible(true);
+        redPanel.add(label);
+        frame.add(redPanel);
+        frame.add(bluePanel);
+        frame.add(greenPanel);
+    }
+}
